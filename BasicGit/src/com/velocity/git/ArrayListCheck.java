6c08807bd27cd79bcb772a71e29452d5369d7d9a
unknown <klohare7@gmail.com>
2022-09-12 23:44:23 +0530

    Fisrt commit

---
+++ BasicGit/src/com/velocity/git/ArrayListCheck.java
@@ -0,0 +1,16 @@
+package com.velocity.git;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListCheck 
+{
+	public static void main(String[] args) 
+	{
+		List<String> names=Arrays.asList("KIRAN","RAJ","AJAY","LADDU");
+		for (String s : names) 
+		{
+			System.out.println(s);
+		}
+	}
+}
